3671ba4dc9335a5fbac97ff40ad6721614c31d22
Honie <honiemun@protonmail.com>
2021-12-30 15:50:31 -0300

    even more taglines

--- script/tagline.js
+++ script/tagline.js
@@ -4,17 +4,20 @@ var taglines = [
 	"the queen bee",
 	"master of the hive",
 	"the bee's knees",
-	"peaked at neco-arc",
+	"peaked at neco-arc", // @gurenya
 	"graphic design is my passion",
 	"lowercase liege",
-	"would you smooch a bee?",
+	"would you smooch a bee?", // undertale
 	"the bee that turns you gay (no clickbait)",
 	"sexoooooo",
-	"hiveborn homewrecker",
+	"hiveborn homewrecker", // lia edit
 	"banger tweets only",
 	"my little corner of the 'net",
-	"ayyy i'm a fucken' bee",
-	"menace to society"
+	"ayyy i'm a fucken' bee", // bebe
+	"menace to society",
+	"that one person who uses gimp",
+	"a whole ass they/them", // hivecord
+	"all warm and runny" // king gizzard
 ];
 
 let randomTagline = taglines[Math.floor(Math.random() * taglines.length)];
