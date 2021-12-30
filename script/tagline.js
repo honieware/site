7eb3d543c939752ec4564bd24fdadfc981a2e88b
Honie <honiemun@protonmail.com>
2021-12-30 15:46:28 -0300

    More taglines

--- script/tagline.js
+++ script/tagline.js
@@ -7,7 +7,14 @@ var taglines = [
 	"peaked at neco-arc",
 	"graphic design is my passion",
 	"lowercase liege",
-	"would you smooch a bee?"
+	"would you smooch a bee?",
+	"the bee that turns you gay (no clickbait)",
+	"sexoooooo",
+	"hiveborn homewrecker",
+	"banger tweets only",
+	"my little corner of the 'net",
+	"ayyy i'm a fucken' bee",
+	"menace to society"
 ];
 
 let randomTagline = taglines[Math.floor(Math.random() * taglines.length)];
